32c56df173400c5546a5588606548062ce70eff3
Palermo <palermo@bond.co>
2018-01-12 15:18:47 -0500

    express server

--- crawler.js
+++ crawler.js
@@ -1,6 +1,3 @@
-'use strict';
-
-const Hapi = require('hapi');
 require('dotenv').config();
 
 var Crawler = require("crawler");
@@ -8,75 +5,161 @@ var _LIST='';
 var _BinanceUrl= 'https://support.binance.com/hc/en-us/categories/115000056351-Announcements';
 
 var CronJob = require('cron').CronJob;
-var Zport = process.env.PORT || 80;
 
 
+var client = require('twilio')(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
-var server = new Hapi.Server(~~process.env.PORT || 3000, '0.0.0.0');
+// function letsMakeSomeMoney() {
+//   var crawler = new Crawler({
+//     maxConnections : 10,
+//     callback : function (error, res, done) {
+//       if (res.statusCode !== 200){
+//         return sendSmsMessage('Crypto Alert: URL responding with ' + res.statusCode + ' status code.')
+//       }
+//
+//       if (error) return sendSmsMessage('Crypto Alert: URL ERROR ' + error);
+//
+//       var $ = res.$;
+//       var newList = $(".article-list-item").text();
+//
+//       if (_LIST !== newList) {
+//         _LIST = newList
+//         return sendSmsMessage('Crypto Alert: $$$ new coin(s) listed on Binance \n' + _LIST)
+//       }
+//
+//       console.log("Nothing changed ...")
+//       done();
+//
+//
+//     }
+//   });
+//
+//   return crawler.queue(_BinanceUrl);
+// }
+//
+// function sendSmsMessage(message) {
+//   const numbers = [
+//     process.env.CHRIS,
+//     process.env.DAVID,
+//     process.env.PALERMO,
+//     process.env.ZOUHAIR
+//   ];
+//
+//   Promise.all(
+//     numbers.map((number) => {
+//       return client.messages.create({
+//         from: process.env.MESSAGE_SERVICE_ID,
+//         to: number,
+//         body: message,
+//       }).then((message) => console.log(message.sid));
+//     })
+//   );
+// }
+//
+// return new CronJob('*/4 * * * *', function() {
+//   //console.log('You will see this message every 30 minute');
+//   return letsMakeSomeMoney();
+// }, null, true, 'America/Los_Angeles');
+//
+// return letsMakeSomeMoney();
+//
+//
+//
+// 'use strict';
+//
+// const Hapi = require('hapi');
+//
+// const server = new Hapi.Server();
+// server.connection({ port: 3000, host: 'localhost' });
+//
+// server.start((err) => {
+//
+//     if (err) {
+//         throw err;
+//     }
+//
+//     return new CronJob('*/4 * * * *', function() {
+//       //console.log('You will see this message every 30 minute');
+//       return letsMakeSomeMoney();
+//     }, null, true, 'America/Los_Angeles');
+//     console.log(`Server running at: ${server.info.uri}`);
+//
+// });
 
-server.start((err) => {
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
 
-    if (err) {
-        throw err;
+const cors = require('cors')
+
+//DB setup
+
+
+//App setup
+app.use(morgan('combine'));
+app.use(cors())
+app.use(bodyParser.json({type: '*/*'}));
+
+
+//Sever setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server litsening on ', port);
+
+function letsMakeSomeMoney() {
+  var crawler = new Crawler({
+    maxConnections : 10,
+    callback : function (error, res, done) {
+      if (res.statusCode !== 200){
+        return sendSmsMessage('Crypto Alert: URL responding with ' + res.statusCode + ' status code.')
+      }
+
+      if (error) return sendSmsMessage('Crypto Alert: URL ERROR ' + error);
+
+      var $ = res.$;
+      var newList = $(".article-list-item").text();
+
+      if (_LIST !== newList) {
+        _LIST = newList
+        return sendSmsMessage('Crypto Alert: $$$ new coin(s) listed on Binance \n' + _LIST)
+      }
+
+      console.log("Nothing changed ...")
+      done();
+
+
     }
-    console.log(`Server running at: ${server.info.uri}`);
+  });
 
-    var client = require('twilio')(
-      process.env.TWILIO_ACCOUNT_SID,
-      process.env.TWILIO_AUTH_TOKEN
-    );
+  return crawler.queue(_BinanceUrl);
+}
 
-    function letsMakeSomeMoney() {
-      var crawler = new Crawler({
-        maxConnections : 10,
-        callback : function (error, res, done) {
-          if (res.statusCode !== 200){
-            return sendSmsMessage('Crypto Alert: URL responding with ' + res.statusCode + ' status code.')
-          }
+function sendSmsMessage(message) {
+  const numbers = [
+    process.env.CHRIS,
+    process.env.DAVID,
+    process.env.PALERMO,
+    process.env.ZOUHAIR
+  ];
 
-          if (error) return sendSmsMessage('Crypto Alert: URL ERROR ' + error);
+  Promise.all(
+    numbers.map((number) => {
+      return client.messages.create({
+        from: process.env.MESSAGE_SERVICE_ID,
+        to: number,
+        body: message,
+      }).then((message) => console.log(message.sid));
+    })
+  );
+}
 
-          var $ = res.$;
-          var newList = $(".article-list-item").text();
-
-          if (_LIST !== newList) {
-            _LIST = newList
-            return sendSmsMessage('Crypto Alert: $$$ new coin(s) listed on Binance \n' + _LIST)
-          }
-
-          console.log("Nothing changed ...")
-          done();
-
-
-        }
-      });
-
-      return crawler.queue(_BinanceUrl);
-    }
-
-    function sendSmsMessage(message) {
-      const numbers = [
-        process.env.CHRIS,
-        process.env.DAVID,
-        process.env.PALERMO,
-        process.env.ZOUHAIR
-      ];
-
-      Promise.all(
-        numbers.map((number) => {
-          return client.messages.create({
-            from: process.env.MESSAGE_SERVICE_ID,
-            to: number,
-            body: message,
-          }).then((message) => console.log(message.sid));
-        })
-      );
-    }
-
-    return new CronJob('*/4 * * * *', function() {
-      //console.log('You will see this message every 30 minute');
-      return letsMakeSomeMoney();
-    }, null, true, 'America/Los_Angeles');
-
-    //return letsMakeSomeMoney();
-});
+return new CronJob('*/4 * * * *', function() {
+  //console.log('You will see this message every 30 minute');
+  return letsMakeSomeMoney();
+}, null, true, 'America/Los_Angeles');
